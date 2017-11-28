e094a37c871d37098c00450492ca81dabfeb62b8
Mohammad Iqbal <iqbalhood@gmail.com>
2017-11-28 14:51:27 +0700

    Ubah Database

--- folderdatabase/klinikusu.sql
+++ folderdatabase/klinikusu.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 27, 2017 at 09:31 AM
+-- Generation Time: Nov 28, 2017 at 08:49 AM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 5.6.28
 
@@ -23,6 +23,50 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `data_dokter`
+--
+
+CREATE TABLE `data_dokter` (
+  `id` int(8) NOT NULL,
+  `nama` varchar(300) NOT NULL,
+  `jenis_kelamin` varchar(1) NOT NULL,
+  `nomor_hp` varchar(24) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `data_dokter`
+--
+
+INSERT INTO `data_dokter` (`id`, `nama`, `jenis_kelamin`, `nomor_hp`) VALUES
+(1, 'dr. H. Boyke Dian Nugraha, SpOG MARS', '1', '081285000336'),
+(2, 'Teuku Adifitrian', '1', '081285000335');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_obat`
+--
+
+CREATE TABLE `data_obat` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(300) NOT NULL,
+  `quantity` varchar(300) NOT NULL,
+  `satuan` varchar(300) NOT NULL,
+  `harga` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `data_obat`
+--
+
+INSERT INTO `data_obat` (`id`, `nama`, `quantity`, `satuan`, `harga`) VALUES
+(1, 'KOKAIN', '15', 'Papan', 2000000),
+(2, 'Panadol', '15', 'Botol 500 Ml', 300000),
+(3, 'K-LINK KLOROPHYLL', '15', 'Botol 800 Mg', 600000);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `data_pasien`
 --
 
@@ -38,12 +82,10 @@ CREATE TABLE `data_pasien` (
 --
 
 INSERT INTO `data_pasien` (`id`, `nama`, `jenis_kelamin`, `nomor_hp`) VALUES
-(1, 'PARTO', '1', '082368008333'),
-(2, 'Mirna Martina', '2', '080898999443'),
-(3, 'Tika Lestari', '2', '08971349414'),
 (18, 'Markus Horison', '1', '082368008333'),
 (19, 'Dwi Agustini Lubis', '2', '0897737373'),
-(20, 'Dwi Andini', '2', '08973701175');
+(20, 'Dwi Andini', '2', '08973701175'),
+(21, 'Bari Subarjo', '1', '0823680084444');
 
 -- --------------------------------------------------------
 
@@ -71,6 +113,18 @@ INSERT INTO `dca_users` (`id`, `username`, `password`, `akses`) VALUES
 --
 
 --
+-- Indexes for table `data_dokter`
+--
+ALTER TABLE `data_dokter`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `data_obat`
+--
+ALTER TABLE `data_obat`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `data_pasien`
 --
 ALTER TABLE `data_pasien`
@@ -87,10 +141,20 @@ ALTER TABLE `dca_users`
 --
 
 --
+-- AUTO_INCREMENT for table `data_dokter`
+--
+ALTER TABLE `data_dokter`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `data_obat`
+--
+ALTER TABLE `data_obat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
 -- AUTO_INCREMENT for table `data_pasien`
 --
 ALTER TABLE `data_pasien`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
