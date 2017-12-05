4f22c0b59e760f2252f2862b7dc549b5b5a8dd3a
Mohammad Iqbal <iqbalhood@gmail.com>
2017-12-05 16:37:59 +0700

    ganti struktur

---
+++ src/admin/app.js
@@ -0,0 +1,54 @@
+(function(){
+    "use strict";
+
+    var app = angular.module("myApp", ["ngClickCopy","myApp.controller","ngRoute", "ngCookies",]);
+    app.config(function($routeProvider) {
+        $routeProvider
+        .when("/home", {
+            templateUrl : "dashboard.html",
+            controller  : "HomeCtrl",
+        })
+        .when("/login", {
+            templateUrl : "login.html",
+            controller  : "LoginCtrl",
+        })
+        .when("/pasien", {
+            templateUrl : "pasien.html",
+            controller  : "PasienCtrl",
+        })
+        .when("/apotik", {
+          templateUrl : "apotik.html",
+          controller  : "ApotikCtrl",
+        })
+        .when("/obat", {
+          templateUrl : "obat.html",
+          controller  : "ObatCtrl",
+        })
+        .when("/dokter", {
+          templateUrl : "dokter.html",
+          controller  : "DokterCtrl",
+        })
+        .when("/users", {
+          templateUrl : "users.html",
+          controller  : "UsersCtrl",
+        })
+        .when("/detail-users/:id", {
+          templateUrl : "data-pasien.html",
+          controller  : "DetailUsersCtrl",
+        })
+        .when("/rekam-medis", {
+          templateUrl : "rekam-medis.html",
+          controller  : "RekamMedisCtrl",
+        })
+        .when("/klinik/:id", {
+          templateUrl : "klinik.html",
+          controller  : "KlinikCtrl",
+        })
+        .otherwise({
+            templateUrl : "dashboard.html",
+            controller  : "HomeCtrl",
+        });
+    });
+
+    
+})();
