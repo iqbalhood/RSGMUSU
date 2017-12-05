87af0282bac03bc623de55c8d5abab505dac7d3f
fahrulhidayat <fahrulhidayat@gmail.com>
2017-12-05 17:34:30 +0700

    fitur crud obat
    tes

--- apidb/obat/postedit.php
+++ apidb/obat/postedit.php
@@ -8,22 +8,23 @@ $postdata = file_get_contents("php://input");
 if(isset($postdata) && !empty($postdata))
 {
     $request  = json_decode($postdata);
-    
+    $newId  = preg_replace('/[^0-9 ]/','',$request->newId);    
     $newName  = preg_replace('/[^a-zA-Z ]/','',$request->newName);
     $newQuantity = preg_replace('/[^0-9 ]/','',$request->newQuantity);
     $newSatuan = preg_replace('/[^a-zA-Z]/','',$request->newSatuan);
 	$newHarga = preg_replace('/[^0-9 ]/','',$request->newHarga);
     
-    if($newName  == '' ||  $newQuantity == '' ||  $newSatuan == ''||  $newHarga == '' ) return;
+    if($newName  == '' ||  $newQuantity == '' ||  $newSatuan == ''||  $newHarga == '') return;
     
     $newName  = mysqli_real_escape_string($connect,$newName);
     $newQuantity = mysqli_real_escape_string($connect,$newQuantity);
 	$newSatuan  = mysqli_real_escape_string($connect,$newSatuan);
     $newHarga = mysqli_real_escape_string($connect,$newHarga);
 
-    $sql = "INSERT INTO `data_obat` (`id`, `nama`, `quantity`, `satuan`, `harga`) VALUES (NULL, '$newName', '$newQuantity', '$newSatuan', '$newHarga')";
+    $sql = "UPDATE `data_obat` SET `nama` = '$newName', `quantity` = '$newQuantity', `satuan` = '$newSatuan',`harga` = '$newHarga' WHERE `data_obat`.`id` = $newId;";
 
     mysqli_query($connect,$sql);
+
     
 
 }
--- src/admin/controller.js
+++ src/admin/controller.js
@@ -335,13 +335,112 @@ app.controller("ObatCtrl", function ($scope,$interval,$http, $route,$timeout, $r
     $scope.shForm   = false;
     $scope.id ="";
 
-   // $http.get("config/daftar_pasien.php").then(function (response) {
+  
    $http.get("../apidb/obat/list_data.php").then(function (response) {
         $scope.myData = response.data.event;
         console.log(response.data.event);
    });
    
-   
+   $scope.showForm = function() {
+            $scope.shTable  = false;
+            $scope.shForm   = true;
+        };
+		
+		$scope.editForm = function(x) {
+            $scope.shTable  = false;
+            $scope.shForm   = true;
+			$scope.id = x;
+			$http({
+                method: 'POST',    
+                url: '../apidb/obat/get.php',
+                data: {newId: x}
+            }).then(function (response) {
+                console.log(response);
+                // on success
+                $scope.people           = response.data;
+                $scope.id               =  $scope.people.id;
+                $scope.name             =  $scope.people.name;
+                $scope.quantity            =  $scope.people.quantity;
+                $scope.satuan          =  $scope.people.satuan;
+                $scope.harga          =  $scope.people.harga;
+                
+            }, function (response) {
+                
+                // on error
+                console.log(response.data,response.status);
+                
+            });
+        };
+		
+		$scope.deleteForm = function(x) {
+       // console.log("This Is delete x value "+x);
+        $http({
+            
+            method: 'POST',
+            url:  '../apidb/obat/delete.php',
+            data: { recordId : x }
+            
+        }).then(function (response) {
+      
+           $route.reload();  
+      
+        }, function (response) {
+            
+            console.log(response.data,response.status);
+            
+        });
+      };
+
+    $scope.cancelForm = function() {
+        
+        $scope.shTable  = true;
+        $scope.shForm   = false;
+        $scope.id               =  "";
+        $scope.name             =  "";
+        $scope.quantity            =  "";
+        $scope.satuan          =  "";
+		$scope.harga          =  "";
+        
+    };
+		
+		$scope.submitForm = function(){
+			if($scope.id){
+            console.log(" ID YANG DIEDIT "+ $scope.id);
+            console.log($scope.id);
+            console.log($scope.name);
+            console.log($scope.kelamin);
+            console.log($scope.phone);
+            $http({
+                
+                 method: 'POST',
+                 url:  '../apidb/obat/postedit.php',
+                 data: {newId: $scope.id, newName: $scope.name, newQuantity: $scope.quantity  , newSatuan: $scope.satuan, newHarga: $scope.harga}
+                 
+            }).then(function (response) {
+
+                console.log(response);
+                // on success
+                if(response.status==200){
+                    $route.reload();    
+                }
+            });
+        }else{
+			$http({
+                
+                 method: 'POST',
+                 url:  '../apidb/obat/post.php',
+                 data: {newName: $scope.name, newQuantity: $scope.quantity  , newSatuan: $scope.satuan, newHarga: $scope.harga}
+                 
+            }).then(function (response) {
+                // on success
+                if(response.status==200){
+                    $route.reload();    
+                }
+            });
+		}
+		};
+		
+	
 
    setTimeout(function(){
     $('#mytableObat').dataTable({
