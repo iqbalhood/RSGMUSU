ce7acad70718e4e8b067c45db9bc72545ba050e9
iqbalhood <iqbalhood@gmail.com>
2018-01-16 15:51:32 +0800

    update query dashboard

--- apidb/antrian/list_data.php
+++ apidb/antrian/list_data.php
@@ -9,15 +9,15 @@ require_once '../../config/db_connect.php';
 $db = new DB_CONNECT();
 	 
 	//  get by event
-	$result = mysql_query("SELECT COUNT(*) FROM tabel_kunjugan WHERE id_klinik = 1") or die(mysql_error());
-	$result2 = mysql_query("SELECT COUNT(*) FROM tabel_kunjugan WHERE id_klinik = 2") or die(mysql_error());
-	$result3 = mysql_query("SELECT COUNT(*) FROM tabel_kunjugan WHERE id_klinik = 3") or die(mysql_error());
-	$result4 = mysql_query("SELECT COUNT(*) FROM tabel_kunjugan WHERE id_klinik = 4") or die(mysql_error());
-	$result5 = mysql_query("SELECT COUNT(*) FROM tabel_kunjugan WHERE id_klinik = 5") or die(mysql_error());
-	$result6 = mysql_query("SELECT COUNT(*) FROM tabel_kunjugan WHERE id_klinik = 6") or die(mysql_error());
-	$result7 = mysql_query("SELECT COUNT(*) FROM tabel_kunjugan WHERE id_klinik = 7") or die(mysql_error());
-	$result8 = mysql_query("SELECT COUNT(*) FROM tabel_kunjugan WHERE id_klinik = 8") or die(mysql_error());
-	$result9 = mysql_query("SELECT COUNT(*) FROM tabel_kunjugan WHERE id_klinik = 9") or die(mysql_error());
+	$result = mysql_query("SELECT COUNT(*) FROM tabel_kunjugan  WHERE `id_klinik` = 1 AND `status` = '1' ") or die(mysql_error());
+	$result2 = mysql_query("SELECT COUNT(*) FROM tabel_kunjugan WHERE `id_klinik` = 2 AND `status` = '1' ") or die(mysql_error());
+	$result3 = mysql_query("SELECT COUNT(*) FROM tabel_kunjugan WHERE `id_klinik` = 3 AND `status` = '1' ") or die(mysql_error());
+	$result4 = mysql_query("SELECT COUNT(*) FROM tabel_kunjugan WHERE `id_klinik` = 4 AND `status` = '1' ") or die(mysql_error());
+	$result5 = mysql_query("SELECT COUNT(*) FROM tabel_kunjugan WHERE `id_klinik` = 5 AND `status` = '1' ") or die(mysql_error());
+	$result6 = mysql_query("SELECT COUNT(*) FROM tabel_kunjugan WHERE `id_klinik` = 6 AND `status` = '1' ") or die(mysql_error());
+	$result7 = mysql_query("SELECT COUNT(*) FROM tabel_kunjugan WHERE `id_klinik` = 7 AND `status` = '1' ") or die(mysql_error());
+	$result8 = mysql_query("SELECT COUNT(*) FROM tabel_kunjugan WHERE `id_klinik` = 8 AND `status` = '1' ") or die(mysql_error());
+	$result9 = mysql_query("SELECT COUNT(*) FROM tabel_kunjugan WHERE `id_klinik` = 9 AND `status` = '1' ") or die(mysql_error());
 	$datapasien = mysql_query("SELECT COUNT(*) FROM data_pasien") or die(mysql_error());
 		// cek
 	
